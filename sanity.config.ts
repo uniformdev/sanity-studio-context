fdb2c2ebad180a5060e530706699bf8d480a588e
Alex Shyba <alex.shyba@uniform.dev>
2024-12-12 19:35:40 -0800

    chore: sanity studio update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,24 +1,40 @@
-import {createConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {schemaTypes} from './schemas'
-import {uniformPlugin} from '@uniformdev/sanity-plugin-uniform'
+import { defineConfig } from "sanity";
+import { schemaTypes } from './schemas'
+import { structureTool } from "sanity/structure";
+import { uniformPlugin } from '@uniformdev/sanity-plugin-uniform'
 
-export default createConfig({
-  name: 'default',
-  title: 'UniformConf',
-  projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: import.meta.env.SANITY_STUDIO_DATASET,
-
+export default defineConfig({
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
   plugins: [
+    structureTool(),
     uniformPlugin({
-      apiHost: import.meta.env.SANITY_STUDIO_UNIFORM_HOST || 'https://uniform.app',
-      apiKey: import.meta.env.SANITY_STUDIO_UNIFORM_API_KEY!,
-      projectId: import.meta.env.SANITY_STUDIO_UNIFORM_PROJECT_ID!,
+      apiHost: 'https://uniform.app',
+      apiKey: process.env.SANITY_STUDIO_UNIFORM_API_KEY!,
+      projectId: process.env.SANITY_STUDIO_UNIFORM_PROJECT_ID!,
     }),
-    deskTool(),
   ],
-
   schema: {
     types: schemaTypes,
   },
-})
+});
+
+// export default createConfig({
+//   name: 'default',
+//   title: 'UniformConf',
+//   projectId: import.meta.env.SANITY_STUDIO_PROJECT_ID,
+//   dataset: import.meta.env.SANITY_STUDIO_DATASET,
+
+//   plugins: [
+//     uniformPlugin({
+//       apiHost: import.meta.env.SANITY_STUDIO_UNIFORM_HOST || 'https://uniform.app',
+//       apiKey: import.meta.env.SANITY_STUDIO_UNIFORM_API_KEY!,
+//       projectId: import.meta.env.SANITY_STUDIO_UNIFORM_PROJECT_ID!,
+//     }),
+//     deskTool(),
+//   ],
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+// })
